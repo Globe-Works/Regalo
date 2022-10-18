2a3a508521b1196ace33c90ffc91257452dc83e5
Matthew Garza <mattg614@gmail.com>
2022-10-18 11:39:44 -0400

    Connect to SQL DB

--- server/controllers/giftController.js
+++ server/controllers/giftController.js
@@ -1,1 +1,6 @@
 const db = require('../models/giftModel');
+
+const giftController = {};
+
+const queryString = `SELECT * FROM users`;
+db.query(queryString).then((data) => console.log(data));
--- server/models/giftModel.js
+++ server/models/giftModel.js
@@ -1,4 +1,13 @@
 const { Pool } = require('pg');
-const { ENV } = require('../.env');
-const PG_URI = `postgres://${ENV.pgSQLUserName}:${pgSQLPass}@peanut.db.elephantsql.com/eyrrjpct`;
+const { pgSQLUserName, pgSQLPass } = require('../../.env');
+
+console.log(pgSQLUserName, pgSQLPass);
+const PG_URI = `postgres://${pgSQLUserName}:${pgSQLPass}@peanut.db.elephantsql.com/${pgSQLUserName}`;
 const pool = new Pool({ connectionString: PG_URI });
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('server/models/giftmodels - executed query:', text);
+    return pool.query(text, params, callback);
+  },
+};
