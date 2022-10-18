a472f1fb1f523870809c95deb7d99aaccfb57351
Jeremy Vogel <jeremyvogel@Jeremys-MBP.fios-router.home>
2022-10-18 11:15:30 -0400

    recipient component v1

---
+++ src/Recipient.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Recipient = (props) => {
+
+    return(<div>
+        <img src={props.img}></img>
+        <span>
+            <h2>{props.firstName} {props.lastName}</h2>
+            <p>Birthday: {props.birthday}</p>
+            <p>Notes: {props.notes}</p>
+        </span>
+    </div>);
+}
